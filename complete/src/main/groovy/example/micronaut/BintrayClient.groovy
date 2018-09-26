2db423a076d1f5f28938c320481c323b72c0e102
Iván López <lopez.ivan@gmail.com>
2018-09-26 15:10:19 +0200

    Change @Client package

--- complete/src/main/groovy/example/micronaut/BintrayClient.groovy
+++ complete/src/main/groovy/example/micronaut/BintrayClient.groovy
@@ -2,7 +2,7 @@ package example.micronaut
 
 import groovy.transform.CompileStatic
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.client.Client
+import io.micronaut.http.client.annotation.Client
 import io.reactivex.Flowable
 
 @CompileStatic
